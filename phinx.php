91aaf15c8e72ed9dfa18e92fc3e85d0f3f3fd8ef
Jamie Long <jamielong45@hotmail.com>
2026-04-19 22:00:44 +0100

    Remove production and testing environments

--- phinx.php
+++ phinx.php
@@ -12,15 +12,6 @@ return
     'environments' => [
         'default_migration_table' => 'phinxlog',
         'default_environment' => 'development',
-        'production' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'production_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ],
         'development' => [
             'adapter' => 'mysql',
             'host'    => $_ENV['MYSQL_HOST'],
@@ -30,15 +21,6 @@ return
             'port'    => 3306,
             'charset' => 'utf8',
         ],
-        'testing' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'testing_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ]
     ],
     'version_order' => 'creation'
 ];
